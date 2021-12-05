81d0cb6ab91d8829bc558d79daeaa90d1b02e969
Rabab Zabir <rzabir@umd.edu>
2021-12-04 23:41:16 -0500

    GenerateTransaction

--- src/main/test/Tests/GenerateTransactions.java
+++ src/main/test/Tests/GenerateTransactions.java
@@ -1,9 +1,49 @@
 package Tests;
+import model.Transaction;
 
 public class GenerateTransactions {
-	
+
+	Set<Integer> readset = new HashSet<>();
+	Set<Integer> writeset = new HashSet<>();
+
+	public Transaction genTrans(int length;int contention){
+
+		Random random = new Random();
+		if(contention == 0){
+			for(int i=0; i<=length; i++){
+				int read = (random.nextInt()) % DATABASE_SIZE;
+				readset.add(read);
+			}
+
+			for(int i=0; i<=length; i++){
+				int write = (random.nextInt()) % DATABASE_SIZE;
+				writeset.add(write);
+			}
+
+		}
+		else if(contention == 1){
+			for(int i =0; i<=length; i++){
+				int read = (random.nextInt()) % 100;
+				readset.add(read);
+			}
+			for(int i=0; i<=length; i++){
+				int write = (random.nextInt()) % 100;
+				writeset.add(write);
+			}
+		}
+		return new Transaction(readset,writeset,true);
+	}
+
+	public List<Transaction> listoftxns(int length;int contention){
+		for(int i=0; i<=PROCESSING_QUEUE_SIZE; i++){
+			List<Transaction> transactionList = new List<Transaction>();
+			transactionList.add(genTrans(int length;int contention));
+		}
+	}
+
 	String type;
-	
+
+
 	
 
 }
--- src/main/test/Tests/Test1.java
+++ src/main/test/Tests/Test1.java
@@ -1,6 +1,13 @@
 package Tests;
-
+@RunWith(TestRunner.class)
 public class Test1 {
+
+    @Test
+    public void test1()
+    {
+        List<Transaction> l1 = listoftxns(30,1);
+        Partition.sendToProcessingQueue(l1);
+    }
 	
 	
 
