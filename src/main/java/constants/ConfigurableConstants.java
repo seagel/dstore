0f701288d0a0d114ca352cb518c359faf5da467f
Manaswini Swamy <manaswiniswamy@wireless-10-104-249-132.umd.edu>
2021-12-05 09:15:22 -0500

    added the changes

--- src/main/java/constants/ConfigurableConstants.java
+++ src/main/java/constants/ConfigurableConstants.java
@@ -4,5 +4,5 @@ public class ConfigurableConstants {
 	public static final int ZOOKEEPER_BATCH_READ_SIZE = 10;
 	public static final int NUMBER_OF_PARTITIONS = 10;
 	public static final int PROCESSING_QUEUE_SIZE = 1000;
-	public static final int DATABASE_SIZE = 1000;
+	public static final int DATABASE_SIZE = 10;
 }
--- src/main/test/Tests/GenerateTransactions.java
+++ src/main/test/Tests/GenerateTransactions.java
@@ -1,32 +1,44 @@
 package Tests;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import constants.ConfigurableConstants;
 import model.Transaction;
 
 public class GenerateTransactions {
 
-	Set<Integer> readset = new HashSet<>();
-	Set<Integer> writeset = new HashSet<>();
-
-	public Transaction genTrans(int length;int contention){
-
+	
+	
+	public Transaction genTrans(int length, int contention)
+	{
+		Set<Integer> readset = new HashSet<Integer>();
+		Set<Integer> writeset = new HashSet<Integer>();
+		
 		Random random = new Random();
-		if(contention == 0){
-			for(int i=0; i<=length; i++){
-				int read = (random.nextInt()) % DATABASE_SIZE;
+		if(contention == 0)
+		{
+			for(int i=0; i<length; i++){
+				int read = (random.nextInt()) % ConfigurableConstants.DATABASE_SIZE;
 				readset.add(read);
 			}
 
 			for(int i=0; i<=length; i++){
-				int write = (random.nextInt()) % DATABASE_SIZE;
+				int write = (random.nextInt()) % ConfigurableConstants.DATABASE_SIZE;
 				writeset.add(write);
 			}
+			
 
 		}
-		else if(contention == 1){
-			for(int i =0; i<=length; i++){
+		else if(contention == 1)
+		{
+			for(int i =0; i<length; i++){
 				int read = (random.nextInt()) % 100;
 				readset.add(read);
 			}
-			for(int i=0; i<=length; i++){
+			for(int i=0; i<length; i++){
 				int write = (random.nextInt()) % 100;
 				writeset.add(write);
 			}
@@ -34,16 +46,15 @@ public class GenerateTransactions {
 		return new Transaction(readset,writeset,true);
 	}
 
-	public List<Transaction> listoftxns(int length;int contention){
-		for(int i=0; i<=PROCESSING_QUEUE_SIZE; i++){
-			List<Transaction> transactionList = new List<Transaction>();
-			transactionList.add(genTrans(int length;int contention));
+	public List<Transaction> listoftxns(int length, int contention)
+	{
+		List<Transaction> transactionList = new ArrayList<Transaction>();
+		for(int i=0; i<=ConfigurableConstants.PROCESSING_QUEUE_SIZE; i++)
+		{
+			transactionList.add(genTrans(length,contention));
 		}
+		return transactionList;
 	}
 
-	String type;
-
-
-	
 
 }
