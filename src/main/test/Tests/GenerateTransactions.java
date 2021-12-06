a049639afe4725681f8684dd08abdb045cf0fd60
Manaswini Swamy <manaswiniswamy@wireless-10-104-249-132.umd.edu>
2021-12-05 20:46:49 -0500

    added percentage_read_write

--- src/main/test/Tests/GenerateTransactions.java
+++ src/main/test/Tests/GenerateTransactions.java
@@ -19,19 +19,21 @@ public class GenerateTransactions {
 	 * @param mode
 	 * @return
 	 */
-	public Transaction genTrans(int length, float contention, int mode)
+	public Transaction genTrans(int length, float percentageRead, float contention, int mode)
 	{
 		Set<Integer> readset = new HashSet<Integer>();
 		Set<Integer> writeset = new HashSet<Integer>();
 		
 		Random random = new Random();
-		for(int i=0; i<length; i++)
+		int lengthRead = (int) (percentageRead*length/100);
+		int lengthReadWrite = (int)((1-percentageRead)*length/100);
+		for(int i=0; i< lengthRead; i++)
 		{
 			int read = (int) ((random.nextInt()) % (100 + (ConfigurableConstants.DATABASE_SIZE-100)*(1-contention)));
 			readset.add(read);
 		}
 		if(mode == 1)
-			for(int i=0; i<=length; i++)
+			for(int i=0; i<lengthReadWrite; i++)
 			{
 				int write = (int) ((random.nextInt()) % (100 + (ConfigurableConstants.DATABASE_SIZE-100)*(1-contention)));
 				writeset.add(write);
@@ -39,12 +41,12 @@ public class GenerateTransactions {
 		return new Transaction(readset,writeset,true);
 	}
 
-	public List<Transaction> listoftxns(int length, float contention, int mode)
+	public List<Transaction> listoftxns(int length, float percentageRead, float contention, int mode)
 	{
 		List<Transaction> transactionList = new ArrayList<Transaction>();
 		for(int i=0; i<=ConfigurableConstants.PROCESSING_QUEUE_SIZE; i++)
 		{
-			transactionList.add(genTrans(length,contention,mode));
+			transactionList.add(genTrans(length,percentageRead,contention,mode));
 		}
 		return transactionList;
 	}
